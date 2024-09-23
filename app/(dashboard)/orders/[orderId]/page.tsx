ebd6aa64a8a39b9caecf12b8881dee4fe6f50640
Sai Vara Prasad Gandhe <gsaivaraprasad123@gmail.com>
2024-09-23 22:43:49 +0530

    order details update api

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -3,7 +3,7 @@ import { columns } from "@/components/orderItems/OrderItemsColums";
 
 const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
   const res = await fetch(
-    `${process.env.ECOMMERCE_STORE_URL}/api/orders/${params.orderId}`
+    `${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`
   );
   const { orderDetails, customer } = await res.json();
 
